13568c849d768ad1bbbdbf6d98649dd8365d4dcc
Dmitrii Kilishek <mentalclear@gmail.com>
2022-07-09 18:48:25 -0400

    Section 8 completed

--- src/components/MainNav.vue
+++ src/components/MainNav.vue
@@ -10,8 +10,25 @@
           border-b border-solid border-brand-gray-1
         "
       >
-        <a href="/" class="flex items-center h-full text-xl">{{ company }}</a>
-      </div>
+        <a :href="url" class="flex items-center h-full text-xl">{{ company }}</a>
+        <nav class="h-full ml-12">
+          <ul class="flex h-full p-0 m-0 list-none">
+            <li 
+              v-for="link in navLinks" 
+              :key="link" 
+              class="h-full ml-9 first:ml-0"
+              data-test="nav-list-item"
+            >
+              <a 
+                href="" 
+                class="flex items-center h-full py-2.5"                
+              >
+                {{ link }}
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </div>  
     </div>
   </header>
 </template>
@@ -22,6 +39,8 @@ export default {
   data() {
     return {
       company: "Bobo Careers",
+      url: 'https://careers.google.com',
+      navLinks: ['Teams', 'Locations', 'Life at BoBo Corp', 'How we hire', 'Students', 'Jobs']
     };
   },
 };
--- tests/unit/components/MainNav.test.js
+++ tests/unit/components/MainNav.test.js
@@ -6,4 +6,25 @@ describe('MainNav', () => {
     const wrapper = mount(MainNav);
     expect(wrapper.text()).toMatch('Bobo Careers');
   });
+
+  describe('Navigation menu items', () => { 
+    it('Display menu items for navigation', () => {
+      const wrapper = mount(MainNav);
+      const navMenuItems = wrapper.findAll('[data-test=\'nav-list-item\']');
+      const navMenuNames = navMenuItems.map((item)=> item.text());      
+      expect(navMenuNames).toEqual([
+        'Teams',
+        'Locations',
+        'Life at BoBo Corp',
+        'How we hire',
+        'Students',
+        'Jobs'
+      ]);
+    });
+    it('Correct amount of menu items', () => {
+      const wrapper = mount(MainNav);
+      const navMenuItems = wrapper.findAll('[data-test=\'nav-list-item\']');
+      expect(navMenuItems.length).toBe(6);
+    });
+  });
 });
