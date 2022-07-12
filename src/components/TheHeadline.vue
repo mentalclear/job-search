c2ab071d6f4cb880ad6550ad2bb6f3d466b2fae4
Dmitrii Kilishek <mentalclear@gmail.com>
2022-07-11 22:59:11 -0400

    Continue Lesson 146

---
+++ src/components/TheHeadline.vue
@@ -0,0 +1,33 @@
+<template>
+  <h1>{{ action }} for everyone</h1>
+  <h2>Find your next job at Bobo Corp.</h2>
+</template>
+
+<script>
+export default {
+  name: 'TheHeadline',
+  data() {
+    return {
+      action: 'Build',
+      interval: null,
+    };
+  },
+  created() {
+    this.changeTitle();
+  },
+  unmounted() {
+    clearInterval(this.interval);
+  },
+  methods: {
+    changeTitle() {
+      this.interval = setInterval(() => {
+        const actions = ['Build', 'Create', 'Design', 'Code'];
+        const currentActionIndex = actions.indexOf(this.action);
+        const nextActionIndex = (currentActionIndex + 1) % 4;
+        const nextAction = actions[nextActionIndex];
+        this.action = nextAction;
+      }, 3000);
+    },
+  },
+};
+</script>
