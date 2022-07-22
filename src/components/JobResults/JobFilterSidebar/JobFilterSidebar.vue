fcd96c0df69ba51930b7c09821c95fe779ea6935
Dmitrii Kilishek <mentalclear@gmail.com>
2022-07-22 12:58:42 -0400

    At Section 22 lesson 311

--- src/components/JobResults/JobFilterSidebar/JobFilterSidebar.vue
+++ src/components/JobResults/JobFilterSidebar/JobFilterSidebar.vue
@@ -1,16 +1,29 @@
 <template>
   <div class="flex flex-col p-4 bg-white border-r border-solid border-brand-gray-1 w-96">
-    Job Filter Sidebar
+    <section class="pb-5">
+      <div class="flex flex-row justify-between">
+        <h3 class="my-4 text-base font-semibold">
+          What do you want to do?
+        </h3>
+        <div class="flex items-center text-sm">
+          <ActionButton
+            text="Clear Filters"
+            type="secondary"
+          />
+        </div>
+      </div>
+      <TheAccordion />
+    </section>
   </div>
 </template>
 
 <script>
+import ActionButton from '@/components/shared/ActionButton.vue';
+import TheAccordion from '@/components/shared/TheAccordion.vue';
+
 export default {
   name: 'JobFilterSidebar',
+  components: { ActionButton, TheAccordion },
 };
 
 </script>
-
-<style lang="scss" scoped>
-
-</style>
