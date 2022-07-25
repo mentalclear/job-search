90b650e92f7ab14646784b28dda339705fbea167
Dmitrii Kilishek <mentalclear@gmail.com>
2022-07-25 18:54:20 -0400

    Section 27 complete

--- src/playground.js
+++ src/playground.js
@@ -1,8 +1,57 @@
-const jobs = [
-  { title: 'Angular Developer', org: 'Microsoft' },
-  { title: 'Programmer', org: 'Google' },
-  { title: 'Developer', org: 'Microsoft' },
-  { title: 'Developer', org: 'Google' },
-];
+import {
+  toRef, toRefs, reactive, computed,
+} from 'vue';
 
-console.log(jobs.filter(job => job.org === 'Microsoft' || job.title === 'Developer'));
+const person = reactive({
+  firstName: 'Dmitrii',
+  lastName: 'Kilishek',
+});
+
+const justObj = { name: 'Bob', lName: 'Fett' };
+
+console.log(toRefs(justObj));
+
+
+// toRefs takes reactive object only. Will not work for just object
+const refPerson = toRefs(person);
+console.log(person.firstName);
+console.log(refPerson.firstName);
+
+
+// Destructuring removes reactivity from the object
+// const { firstName, lastName } = person;
+
+// Using toRef makes each property reactive
+// const firstName = toRef(person, 'firstName');
+// const lastName = toRef(person, 'lastName');
+
+// Destructuring with Reactivity
+const { firstName, lastName } = toRefs(person);
+
+
+// const title = `${person.value.name} the Great`;
+const title = computed(() => `${firstName.value} ${lastName.value} the Great`);
+
+console.log(title.value);
+
+person.firstName = 'Peter';
+console.log(title.value);
+
+person.lastName = 'Shwarzemuller';
+console.log(title.value);
+
+// const title = `${person.name.value} the Great`;
+// console.log(title);
+
+// person.name = 'Peter';
+// console.log(title);
+
+
+// const name = ref('Dmitrii');
+// console.log(name.value);
+// const title = computed(() => `${name.value} the Great`);
+// // const title = ref(`${name.value} the Great`);
+// console.log(title.value);
+
+// name.value = 'Peter';
+// console.log(title.value);
